2af92736ddb00833615e0f8275017bfc05604e68
gkfktkrh153 <gkfktkrh153@naver.com>
2023-03-27 17:58:51 +0900

    3-10

--- src/main/java/com/mysite/sbb/domain/answer/service/AnswerService.java
+++ src/main/java/com/mysite/sbb/domain/answer/service/AnswerService.java
@@ -1,13 +1,16 @@
 package com.mysite.sbb.domain.answer.service;
 
+import com.mysite.sbb.DataNotFoundException;
 import com.mysite.sbb.domain.answer.model.Answer;
 import com.mysite.sbb.domain.answer.repository.AnswerRepository;
 import com.mysite.sbb.domain.question.model.Question;
 import com.mysite.sbb.domain.user.model.User;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cglib.core.Local;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
@@ -20,5 +23,22 @@ public class AnswerService {
         answer.setQuestion(question);
         answer.setAuthor(author);
         answerRepository.save(answer);
+    }
+    public Answer getAnswer(Long id){
+        Optional<Answer> answer = this.answerRepository.findById(id);
+        if(answer.isPresent()){
+            return answer.get();
+        }
+        else{
+            throw new DataNotFoundException("answer not found");
+        }
+    }
+    public void modify(Answer answer, String content){
+        answer.setContent(content);
+        answer.setModifyDate(LocalDateTime.now());
+        this.answerRepository.save(answer);
+    }
+    public void delete(Answer answer) {
+        this.answerRepository.delete(answer);
     }
 }
